ccfc62ef1e037b263dc46377b393fe539101be1c
Avery Harnish <averyharnish@gmail.com>
2017-10-12 23:04:08 -0400

    added links to navbar and about page updates from README.md

--- routes.py
+++ routes.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flaskext.markdown import Markdown
+
 app = Flask(__name__)
+markdown = Markdown(app)
 
 @app.route('/')
-def hello_world():
+def home():
 	return render_template('index.html')
+
+@app.route('/about/')
+def about():
+	content = open("README.md", "r").read()
+	return render_template('about.html', content=content)
