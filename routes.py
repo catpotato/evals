4a1e36a62a5b6abcfae8a3346e1258625686701e
Clark Addis <catpotato1@gmail.com>
2017-10-20 15:38:27 -0400

    barebones communciation btwn sever and cards

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flaskext.markdown import Markdown
 
 app = Flask(__name__)
@@ -16,3 +16,15 @@ def about():
 @app.route('/compare')
 def compare():
 	return render_template('compare.html')
+
+@app.route('/_send_query')
+def send_query():
+	department = request.args.get('department', False)
+	course = request.args.get('course', False)
+	professor = request.args.get('professor', False)
+
+	print(department)
+	print(course)
+	print(professor)
+
+	return jsonify(department = department, course = course, professor = professor)
--- static/js/compare.js
+++ static/js/compare.js
@@ -1,18 +1,47 @@
 $(function(){
 
   // initial card add
-  $('.card').clone().insertBefore('#plus-button').css('display','inline-block');
+  add_card();
 
-  //
   $('#plus-button').click(function(){
-    // does a bunch, clones the template guy, then makes it noe longer invisible
-    $('.card').first().clone().insertBefore(this).css('display','inline-block')
+    //console.log('hey')
+    add_card()
 
-    // this is close functionality, has to be added here because each new card needs its own close mechanism
-    $('.close').click(function(){
-      $(this).parent().parent().remove()
-    })
+
+
+
+    // this is submit functionality, needs to be added here for the same reason above
+
+
 
   })
 
 })
+
+function add_card(){
+
+  // does a bunch, clones the template guy, then makes it noe longer invisible
+  $('.card').first().clone().insertBefore($('#plus-button')).css('display','inline-block')
+
+  // this is close functionality, has to be added here because each new card needs its own close mechanism
+  $('.close').click(function(){
+    $(this).parent().parent().remove()
+  })
+
+  $('.submit').last().click(function(){
+    console.log('ive been clicked')
+    form = $(this).parent()
+    //console.log($(this).parent().children('input[name="department"]').val())
+
+    $.getJSON($SCRIPT_ROOT + '/_send_query', {
+      department: form.children('input[name="department"]').val(),
+      course: form.children('input[name="course"]').val(),
+      professor: form.children('input[name="professor"]').val()
+    }, function(data) {
+      console.log(data.course);
+    })
+
+  })
+
+
+}
