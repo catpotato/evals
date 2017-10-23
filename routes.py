25d4ee84fdb61a7e8a2c146fdb0c8f897bf6ecb9
EverlastingBugstopper <averyharnish@gmail.com>
2017-10-22 23:31:50 -0400

    Updated form handling to use wtforms and javascript closing scalable

--- routes.py
+++ routes.py
@@ -1,8 +1,24 @@
+from os import urandom
 from flask import Flask, render_template, request, jsonify
 from flaskext.markdown import Markdown
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from processor import Processor
+from query import Query
+from wtforms import HiddenField, StringField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 markdown = Markdown(app)
+csrf.init_app(app)
+app.secret_key = urandom(24).encode('hex')
+
+class ProfessorForm(FlaskForm):
+	department = StringField('', validators=[DataRequired()], render_kw={'placeholder': 'department'})
+	course = StringField('', validators=[DataRequired()], render_kw={'placeholder': 'course number'})
+	professor = StringField('', validators=[DataRequired()], render_kw={'placeholder': 'professor'})
 
 @app.route('/')
 def home():
@@ -13,18 +29,19 @@ def about():
 	content = open("README.md", "r").read()
 	return render_template('about.html', content=content)
 
-@app.route('/compare')
+@app.route('/compare/', methods=['GET', 'POST'])
 def compare():
-	return render_template('compare.html')
+	form = ProfessorForm()
 
-@app.route('/_send_query')
-def send_query():
-	department = request.args.get('department', False)
-	course = request.args.get('course', False)
-	professor = request.args.get('professor', False)
+	if form.validate_on_submit():
+		data = _process_query(department=form.department.data, course=form.course.data, professor=form.professor.data).get_processed_data()
+		print(data)
 
-	print(department)
-	print(course)
-	print(professor)
+	return render_template('compare.html', form=form)
 
-	return jsonify(department = department, course = course, professor = professor)
+def _process_query(department=None, period=None, course=None, professor=None):
+	return Processor(Query(period=period, department=department, course=course, professor=professor))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
